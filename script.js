1bb3e565dc4dff5783e2274a7464d69e7c5b3946
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-01-26 16:51:11 +0600

    add classes

--- script.js
+++ script.js
@@ -7,47 +7,57 @@
 
 const cardImg = document.createElement("img");
 cardImg.src= "/images/image-equilibrium.jpg";
+cardImg.className = "cardImg";
 document.body.appendChild(cardImg);
 
 const title = document.createElement("h3");
 title.innerText = "Equilibrium #3429";
+title.className = "title";
 title.style.color = "hsl(0, 0%, 100%)";
 document.body.appendChild(title);
 
 const subtitle = document.createElement("p");
 subtitle.innerText = "Our Equilibrium collection promotesbalance and calm";
+subtitle.className = "subtitle";
 subtitle.style.color = "hsl(215, 51%, 70%)";
 document.body.appendChild(subtitle);
 
 const etherium = document.createElement("img");
 etherium.src = "/images/icon-ethereum.svg";
+etherium.className = "pricePic";
 document.body.appendChild(etherium);
 
 const price = document.createElement("p");
 price.innerText = "0.041 ETH";
+price.className = "price";
 price.style.color = "hsl(178, 100%, 50%)";
 document.body.appendChild(price);
 
 const clockImg = document.createElement("img");
 clockImg.src = "/images/icon-clock.svg";
+clockImg.className = "clockImg";
 document.body.appendChild(clockImg);
 
 const deadline = document.createElement("p");
 deadline.innerText = "3 days left";
+deadline.className = "deadline";
 deadline.style.color = "hsl(215, 51%, 70%)";
 document.body.appendChild(deadline);
 
 const avatarImg = document.createElement("img");
 avatarImg.src = "/images/image-avatar.png";
+avatar.className = "avatar";
 document.body.appendChild(avatarImg);
 
 const madeBy = document.createElement("p");
 madeBy.innerText = "creation of ";
+madeBy.className = "madeBy";
 madeBy.style.color = "hsl(215, 51%, 70%)";
 document.body.appendChild(madeBy);
 
 const madeByName = document.createElement("p");
 madeByName.innerText = "Jules Wyvern";
+madeByName.className = "madeByName";
 madeByName.style.color = "hsl(0, 0%, 100%)";
 document.body.appendChild(madeByName);
 
