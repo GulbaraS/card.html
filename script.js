1c084270d3ee6019dbe7c92232db3764fbc3a0e2
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-01-26 20:11:44 +0600

    add div for price and deadline

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ card.style.boxShadow =
   " 3px 14px 28px rgba(0,0,0,0.25), 3px 10px 10px rgba(0,0,0,0.22)";
 wrapper.appendChild(card);
 
-
+//card image
 const cardImg = document.createElement("img");
 cardImg.src= "/images/image-equilibrium.jpg";
 cardImg.className = "cardImg";
@@ -32,27 +32,41 @@ cardImg.style.maxWidth = "300px";
 cardImg.style.maxHeight = "300px";
 card.appendChild(cardImg);
 
-
+//card name h3
 const title = document.createElement("h3");
 title.innerText = "Equilibrium #3429";
 title.className = "title";
 title.style.color = "hsl(0, 0%, 100%)";
 card.appendChild(title);
 
-
+//subtitle text 
 const subtitle = document.createElement("p");
-subtitle.innerText = "Our Equilibrium collection promotes balance and calm.";
 subtitle.className = "subtitle";
+subtitle.innerText = "Our Equilibrium collection promotes balance and calm.";
 subtitle.style.color = "hsl(215, 51%, 70%)";
 subtitle.style.fontSize = "18px";
+subtitle.style.fontSize = "300";
+subtitle.style.lineHeight = "25px";
 subtitle.style.width = "300px";
 card.appendChild(subtitle);
 
+//div for price and deadline
+const saleData = document.createElement("div");
+saleData.style.display = "flex";
+saleData.style.justifyContent = "space between";
+saleData.style.marginBottom = "15px";
+card.appendChild(saleData);
+
+//div for price
+let priceDiv = document.createElement("div");
+saleData.appendChild(priceDiv);
 
 const etherium = document.createElement("img");
 etherium.src = "/images/icon-ethereum.svg";
 etherium.className = "pricePic";
-card.appendChild(etherium);
+etherium.style.marginRight = "5px";
+etherium.style.display = "inline";
+priceDiv.appendChild(etherium);
 
 
 const price = document.createElement("p");
@@ -60,21 +74,38 @@ price.innerText = "0.041 ETH";
 price.className = "price";
 price.style.color = "hsl(178, 100%, 50%)";
 price.style.fontSize = "18px";
-card.appendChild(price);
+price.style.margin = "0";
+price.style.display = "inline";
+priceDiv.appendChild(price);
 
+//div fot deadline
+let deadlineDiv = document.createElement("div");
+deadlineDiv.style.marginLeft = "100px";
+saleData.appendChild(deadlineDiv);
 
 const clockImg = document.createElement("img");
 clockImg.src = "/images/icon-clock.svg";
 clockImg.className = "clockImg";
-card.appendChild(clockImg);
+clockImg.style.marginRight = "5px";
+clockImg.style.display = "inline";
+deadlineDiv.appendChild(clockImg);
 
 
 const deadline = document.createElement("p");
 deadline.innerText = "3 days left";
 deadline.className = "deadline";
 deadline.style.color = "hsl(215, 51%, 70%)";
+deadline.style.display = "inline";
 deadline.style.fontSize = "18px";
-card.appendChild(deadline);
+deadlineDiv.appendChild(deadline);
+
+// line
+let line = document.createElement("hr");
+line.style.borderColor = "hsl(215, 32%, 27%";
+line.style.marginBottom = "15px";
+card.appendChild(line);
+
+//
 
 const avatarImg = document.createElement("img");
 avatarImg.src = "/images/image-avatar.png";
